b2d4e327e811bc45c208f8a20e82991bbe228c55
robtomb <robtomb@outlook.com>
2017-08-24 09:01:38 +0800

    [1] read data from another js docu

---
+++ data.js
@@ -0,0 +1,10 @@
+
+module.exports = function getData() {
+	return [{name:'Harry Potter' , id:2011 , class:1 , 
+		math:79 , chinese:89 , english:65.5 , progromming:99 , avg:0 , sum:0 } , 
+			{name:'Pride and Prejudice' , id:2111 , class:1 , 
+		math:89 , chinese:99 , english:96 , progromming:70 , avg:0 , sum:0} ,
+			{name:'Animal Farm' , id:2017 , class:1 , 
+		math:77 , chinese:80 , english:80 , progromming:77 , avg:0 , sum:0}
+		];
+}
