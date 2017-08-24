36455acfb0622b98952c4526a6ac59481e268abf
robtomb <robtomb@outlook.com>
2017-08-24 22:08:04 +0800

    重构

---
+++ menu.js
@@ -0,0 +1,16 @@
+
+module.exports = {menu:menu};
+
+const input = require('./input.js');
+const output = require('./output.js');
+
+const funName = ['input' , 'output'];
+
+function menu(data) {
+
+	const readlineSync = require('readline-sync'),
+		menu = ['添加学生','生成成绩单'];
+		index = readlineSync.keyInSelect(menu, '请输入你的选择:');
+	if( index !== -1 )
+		eval(funName[index])(data);
+}
