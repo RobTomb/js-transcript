8dfb28e38a55485792edf8c7420f4f6edbd0f74f
robtomb <robtomb@outlook.com>
2017-08-24 09:23:56 +0800

    [2] menu was finished you can choose

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 const data = require('./data')();
+
+function input() {
+
+}
+
+function showInfo() {
+
+}
+
+function menu() {
+
+	const readlineSync = require('readline-sync'),
+		menu = ['ADD STUDENT','PRINT TRANSCRIPT'];
+		index = readlineSync.keyInSelect(menu, 'Pleace input your choice:');
+
+	if(index === 0)
+		input();
+	else if(index === 1)
+		showInfo();
+}
+
+function main() {
+	menu();
+}
+
+main();
